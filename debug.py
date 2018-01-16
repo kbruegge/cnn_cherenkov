01ed5659d686e966fc0f10090c85bc79ced25695
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2018-01-16 18:31:30 +0100

    remove old files

--- debug.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import image_io
-
-meta, images = image_io.read_n_rows('./data/crab_images.hdf5',start=0, end=5000 )
-
-import IPython; IPython.embed()
